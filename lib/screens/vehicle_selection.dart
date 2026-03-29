bcf65b33dac68c3b0478fbce3ba979d8510419b1
shahzaiba <shahzaibanoor@gmail.com>
2026-03-29 12:50:33 +0500

    Vehicale selection file added

---
+++ lib/screens/vehicle_selection.dart
@@ -0,0 +1,198 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class VehicleSelectionScreen extends StatefulWidget {
+  final bool fromSettings;
+
+  const VehicleSelectionScreen({super.key, this.fromSettings = false});
+
+  @override
+  State<VehicleSelectionScreen> createState() => _VehicleSelectionScreenState();
+}
+
+class _VehicleSelectionScreenState extends State<VehicleSelectionScreen> {
+  String selectedVehicle = 'Car';
+
+  final TextEditingController driverNameController = TextEditingController();
+  final TextEditingController vehicleNumberController = TextEditingController();
+  final TextEditingController locationController = TextEditingController();
+
+  final List<String> vehicles = ['Car', 'Bike', 'Bus', 'Truck'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/home.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+
+          Container(color: Colors.black.withOpacity(0.45)),
+
+          SafeArea(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(18),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.pop(context),
+                        child: const Icon(
+                          Icons.arrow_back_ios_new,
+                          color: Colors.white,
+                          size: 22,
+                        ),
+                      ),
+                      const SizedBox(width: 15),
+
+                      const Text(
+                        "Vehicle Selection",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const SizedBox(height: 8),
+                  const Text(
+                    "Add driver, vehicle & location details",
+                    style: TextStyle(color: Colors.white70),
+                  ),
+                  const SizedBox(height: 25),
+
+                  glassCard(
+                    child: Column(
+                      children: [
+                        TextField(
+                          cursorColor: Colors.redAccent,
+                          controller: driverNameController,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: inputDecoration("Driver Name"),
+                        ),
+
+                        const SizedBox(height: 15),
+
+                        DropdownButtonFormField<String>(
+                          initialValue: selectedVehicle,
+                          dropdownColor: Colors.black87,
+                          items: vehicles
+                              .map(
+                                (v) => DropdownMenuItem(
+                                  value: v,
+                                  child: Text(
+                                    v,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              )
+                              .toList(),
+                          onChanged: (value) {
+                            setState(() => selectedVehicle = value!);
+                          },
+                          decoration: inputDecoration("Vehicle Type"),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+
+                        const SizedBox(height: 15),
+
+                        TextField(
+                          controller: vehicleNumberController,
+                          cursorColor: Colors.redAccent,
+                          style: const TextStyle(color: Colors.white),
+                          decoration: inputDecoration(
+                            "Vehicle Number (LEA-1234)",
+                          ),
+                        ),
+
+                        const SizedBox(height: 15),
+
+                        const SizedBox(height: 25),
+
+                        SizedBox(
+                          width: double.infinity,
+                          child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.redAccent.withOpacity(
+                                0.7,
+                              ),
+                              padding: const EdgeInsets.symmetric(vertical: 14),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30),
+                              ),
+                            ),
+                            onPressed: () {
+                              ///
+                              if (widget.fromSettings) {
+                                Navigator.pop(context);
+                              } else {
+                                Navigator.pushReplacementNamed(
+                                  context,
+                                  '/main',
+                                );
+                              }
+                            },
+                            child: const Text(
+                              "Save",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget glassCard({required Widget child}) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(22),
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+        child: Container(
+          padding: const EdgeInsets.all(18),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.15),
+            borderRadius: BorderRadius.circular(22),
+            border: Border.all(color: Colors.white.withOpacity(0.3)),
+          ),
+          child: child,
+        ),
+      ),
+    );
+  }
+
+  InputDecoration inputDecoration(String label) {
+    return InputDecoration(
+      labelText: label,
+      labelStyle: const TextStyle(color: Colors.white70),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: BorderSide(color: Colors.white.withOpacity(0.4)),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(14),
+        borderSide: const BorderSide(color: Colors.redAccent),
+      ),
+    );
+  }
+}
