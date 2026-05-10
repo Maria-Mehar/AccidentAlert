9bba4fb7244a1b15a173de64b7311bf114ac939d
Shahzaiba-77 <shahzaibanoor@gmail.com>
2026-05-10 15:20:33 +0500

    Added MethodChannel for native notification in MainActivity
    It is a  bridge of alert system of AcciSense  project.

--- android/app/src/main/kotlin/com/accident_alert/MainActivity.kt
+++ android/app/src/main/kotlin/com/accident_alert/MainActivity.kt
@@ -3,3 +3,23 @@ package com.accident_alert
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity : FlutterActivity()
+
+override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+    super.configureFlutterEngine(flutterEngine)
+
+
+    MethodChannel(flutterEngine.dartExecutor.binaryMessenger, "accident_channel")
+        .setMethodCallHandler { call, result ->
+            if (call.method == "showNotification") {
+                val docId = call.argument<String>("docId")
+                if (docId != null) {
+                    showAccidentNotification(docId) 
+                    result.success("Native Notification Triggered")
+                } else {
+                    result.error("ERROR", "docId missing", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+}
