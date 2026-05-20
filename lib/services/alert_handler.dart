54c64cf7aa34ab94860e16af0752d10697930aa5
qafia-sq <qafiabhattiq@gmail.com>
2026-05-19 16:05:22 -1200

    update alert handler

--- lib/services/alert_handler.dart
+++ lib/services/alert_handler.dart
@@ -1,34 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:AcciSense/screens/alert_screen.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+import 'package:AcciSense/services/notification_bridge.dart';
+
 class AlertHandler {
   static bool _isAlertOpen = false;
 
-  // Day 1: Listen for accidents in real-time
   static void listenForAccidents(BuildContext context) {
     FirebaseFirestore.instance
         .collection('accidents')
+        // .where('userId', isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+        // .where('status', isEqualTo: 'active')
         .snapshots()
         .listen((snapshot) {
+          print("🔥 SNAPSHOT: ${snapshot.docs.length}");
           if (snapshot.docs.isNotEmpty && !_isAlertOpen) {
             final doc = snapshot.docs.first;
-            
-            // Is flag ka maqsad ye hai ke ek sath 10 screens na khul jayein
+
+            print("🔥 DOC ID: ${doc.id}");
+
             _isAlertOpen = true;
 
             Navigator.push(
               context,
               MaterialPageRoute(
                 builder: (_) => AlertScreen(docId: doc.id, data: doc.data()),
-                 builder: (_) => AlertScreen( docId: doc.id,  data: doc.data() as Map<String, dynamic>
-                 )
               ),
             ).then((_) {
-              _isAlertOpen = false; // Screen close hone par wapas false
+              _isAlertOpen = false;
             });
+            NotificationBridge.showNotification(doc.id);
           }
         });
   }
-} // Screen import ki
-
-            
+}
